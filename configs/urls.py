0ee1d98d6d56155efe43cde5b2695acb11067079
KyzukY <kyzuk.yr@gmail.com>
2022-06-23 13:40:54 +0300

    hw_2

--- configs/urls.py
+++ configs/urls.py
@@ -1,10 +1,10 @@
-
 from django.contrib import admin
 from django.urls import path
 
-from userlist.views import UsersListCreateView
+from userlist.views import UsersListCreateView, UserByIdView
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
-    path('users',UsersListCreateView.as_view())
+    path('users', UsersListCreateView.as_view()),
+    path('users/<int:pk>', UserByIdView.as_view()),
 ]
