3a2a7307797910d914057e6f61aa6766c4e767f7
KyzukY <kyzuk.yr@gmail.com>
2022-07-12 16:48:02 +0300

    hw_6

--- apps/auth/urls.py
+++ apps/auth/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from apps.auth.views import ActivateUserView
+from apps.auth.views import ActivateUserView, ChangePasswordView, RecoveryPasswordReqeustView
 
 urlpatterns = [
     path('', TokenObtainPairView.as_view()),
     path('/refresh', TokenRefreshView.as_view()),
     path('/activate/<str:token>', ActivateUserView.as_view()),
+    path('/recovery', RecoveryPasswordReqeustView.as_view()),
+    path('/recovery/<str:token>', ChangePasswordView.as_view()),
 
 ]
--- core/enums/action_enum.py
+++ core/enums/action_enum.py
@@ -4,6 +4,7 @@ from enum import Enum
 
 class ActionEnum(Enum):
     ACTIVATE = ('activate', timedelta(days=1))
+    RECOVERY=('recovery', timedelta(minutes=30))
 
     def __init__(self,token_type,exp_time):
         self.exp_time = exp_time
