5b0fd08c209815c6bc76433ce97f9a8d9b441362
KyzukY <kyzuk.yr@gmail.com>
2022-07-21 12:08:46 +0300

    hw_9

--- backend/configs/urls.py
+++ backend/configs/urls.py
@@ -16,6 +16,7 @@ schema_view = get_schema_view(
         contact=openapi.Contact(email="contact@snippets.local"),
         license=openapi.License(name="BSD License"),
     ),
+
     public=True,
     permission_classes=[AllowAny],
 )
